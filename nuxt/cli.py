9dae21614b6eced91b8a4673ec7c25836c274012
Arvin <arvintian8@gmail.com>
2021-11-30 11:50:37 +0800

    refactor debug mode reload

--- nuxt/cli.py
+++ nuxt/cli.py
@@ -1,56 +1,90 @@
+from nuxt.wsgi_app import app
+from nuxt.utils import getcwd
+from nuxt.reloader import reloader_engines
+from madara.app import Madara
+from gunicorn.app.base import BaseApplication
+from gunicorn.workers.base import Worker
+from copy import deepcopy
+import traceback
+import threading
 import sys
 import click
 import os
 import importlib
-from gunicorn.app.base import BaseApplication
-from .wsgi_app import app
-from .utils import getcwd
-from copy import deepcopy
 import json
+
+
+class WebApplication(BaseApplication):
+
+    def __init__(self, application, module, options=None):
+        self.options: dict = options or {}
+        self.application: Madara = application
+        self.module = module
+        self.setup()
+        super().__init__()
+
+    def setup(self):
+        if self.application.config.get("debug"):
+            self.options.update({
+                "post_fork": self.post_fork
+            })
+
+    def reload_app(self, fname: str):
+        try:
+            should_reload = False
+            for module in tuple(sys.modules.values()):
+                if getattr(module, '__file__', "") == fname:
+                    should_reload = True
+                    break
+            if not should_reload:
+                return
+            # reinit application
+            self.application.__init__(self.application.config)
+            # reload modified module
+            for module in tuple(sys.modules.values()):
+                if module == self.module:
+                    continue
+                if getattr(module, '__file__', "") == fname:
+                    importlib.reload(module)
+            # realod entry module
+            importlib.reload(self.module)
+            self.application.logger.info("Worker reloading: %s modified", fname)
+        except Exception as e:
+            self.application.logger.error("Worker reload error {}".format(traceback.format_exc()))
+
+    def post_fork(self, server, worker: Worker):
+        reloader_cls = reloader_engines["auto"]
+        reloader: threading.Thread = reloader_cls(extra_files=[], callback=self.reload_app)
+        reloader.start()
+        self.application.logger.debug("Worker {} start reload watch threading".format(worker.pid))
+
+    def load_config(self):
+        the_config = {key: value for key, value in self.options.items()
+                      if key in self.cfg.settings and value is not None}
+        for key, value in the_config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
 
 
 def start_server(address, port, workers, module):
     """
     启动 http server
     """
-
-    class WebApplication(BaseApplication):
-
-        def __init__(self, application, options=None):
-            self.options = options or {}
-            self.application = application
-            super().__init__()
-
-        def load_config(self):
-            the_config = {key: value for key, value in self.options.items()
-                          if key in self.cfg.settings and value is not None}
-            for key, value in the_config.items():
-                self.cfg.set(key.lower(), value)
-
-        def load(self):
-            if options.get("preload"):
-                self.application.__init__(self.application.config)
-                importlib.reload(module)
-            return self.application
-
-    options = {
+    gunicorn_options = {
         "bind": "{}:{}".format(address, port),
         "workers": workers,
         "accesslog": "-",
         "errorlog": "-",
+        "reload": False,
+        "preload": False,
     }
     # compatible with gunicorn cfg
-    gunicorn_options = app.config.get("gunicorn", {})
-    if gunicorn_options:
-        options.update(gunicorn_options)
-    # debug mode
-    if app.config.get("debug"):
-        options.update({
-            "reload": True,
-            "preload": True,
-        })
-        app.logger.debug("gunicron config:{}".format(options))
-    WebApplication(app, options).run()
+    gunicorn_cfg = app.config.get("gunicorn", {})
+    if gunicorn_cfg:
+        gunicorn_options.update(gunicorn_cfg)
+    WebApplication(app, module, gunicorn_options).run()
 
 
 def settings(cfg: dict) -> dict:
