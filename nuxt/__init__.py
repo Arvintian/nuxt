8069f3f9ef896a2e2c1b9adecb3415dd19506fc0
Arvin <arvintian8@gmail.com>
2023-02-13 18:21:21 +0800

    hidden asgi route class

--- nuxt/__init__.py
+++ nuxt/__init__.py
@@ -2,7 +2,7 @@ from madara.blueprints import Blueprint
 from madara.wrappers import Request, Response
 from werkzeug.utils import redirect, send_file, send_from_directory
 from starlette.websockets import WebSocket, WebSocketDisconnect
-from starlette.routing import Route
+from starlette.routing import Route as __Route
 from nuxt.templating import render_template, render_html
 from nuxt.utils import to_asgi_pattern as __to_asgi_pattern
 from nuxt.app import wsgi_app as __wsgi_app
@@ -13,7 +13,7 @@ __origin_add_url_rule = __wsgi_app.add_url_rule
 
 
 def __add_url_rule(pattern: str, endpoint=None, view_func=None, provide_automatic_options=None, **options):
-    __asgi_app.routes.append(Route(__to_asgi_pattern(pattern), __wsgi_wapper_app, methods=options.get("methods")))
+    __asgi_app.routes.append(__Route(__to_asgi_pattern(pattern), __wsgi_wapper_app, methods=options.get("methods")))
     return __origin_add_url_rule(pattern, endpoint, view_func, provide_automatic_options, **options)
 
 
